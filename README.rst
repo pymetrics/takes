4b8d43fbf2f9c25172e12a8983a30abee3317d85
Chris Lawlor <chris@pymetrics.com>
2021-11-26 12:23:07 -0500

    Initial import

---
+++ README.rst
@@ -0,0 +1,37 @@
+=====
+Takes
+=====
+
+
+.. image:: https://img.shields.io/pypi/v/takes.svg
+        :target: https://pypi.python.org/pypi/takes
+
+.. image:: https://img.shields.io/travis/pymetrics/takes.svg
+        :target: https://travis-ci.com/pymetrics/takes
+
+.. image:: https://readthedocs.org/projects/takes/badge/?version=latest
+        :target: https://takes.readthedocs.io/en/latest/?version=latest
+        :alt: Documentation Status
+
+
+
+
+Decorator for run-time type checks.
+
+
+* Free software: MIT license
+* Documentation: https://takes.readthedocs.io.
+
+
+Features
+--------
+
+* TODO
+
+Credits
+-------
+
+This package was created with Cookiecutter_ and the `pymetrics/cookiecutter-python-library`_ project template.
+
+.. _Cookiecutter: https://github.com/audreyr/cookiecutter
+.. _`audreyr/cookiecutter-pypackage`: https://github.com/pymetrics/cookiecutter-python-library
---
+++ tests/test_takes.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python
+
+"""Tests for `takes` package."""
+
+from dataclasses import dataclass
+
+import pytest
+
+from takes.takes import takes
+
+
+@dataclass
+class Point:
+    x: int = 0
+    y: int = 0
+
+
+def test_takes_point():
+    X, Y = 1, 1
+
+    @takes(Point)
+    def test(point):
+        assert isinstance(point, Point)
+        assert point.x == X
+        assert point.y == Y
+
+    test(Point(x=X, y=Y))
+    test({"x": X, "y": Y})
+
+
+def test_keeps_args():
+    outer_arg = object()
+
+    @takes(Point)
+    def test(point, arg):
+        assert isinstance(point, Point)
+        assert arg is outer_arg
+
+    test(Point(), outer_arg)
+
+
+def test_keeps_kwargs():
+
+    outer = object()
+
+    @takes(Point)
+    def test(point, kwarg=None):
+        assert isinstance(point, Point)
+        assert kwarg is outer
+
+    test(Point(), kwarg=outer)
+
+
+def test_named_argument():
+    @takes(Point, name="point")
+    def test(arg, point):
+        assert isinstance(point, Point)
+
+    test(object(), Point())
+
+
+def test_kw_only_argument():
+    @takes(Point, name="kw_only")
+    def test(args, *, kw_only=None):
+        assert isinstance(kw_only, Point)
+
+    test(1, kw_only={"x": 1, "y": 1})
+
+
+def test_pos_or_kwargs_called_as_pos():
+    @takes(Point, name="pos_or_kw_2")
+    def test(pos_or_kw_1, pos_or_kw_2):
+        assert isinstance(pos_or_kw_2, Point)
+
+    test(1, {"x": 1, "y": 1})
+
+
+def test_first_pos_or_kwarg_called_as_kwarg():
+    @takes(Point, name="pos_or_kw_2")
+    def test(pos_or_kw_1, pos_or_kw_2):
+        assert isinstance(pos_or_kw_2, Point)
+
+    test(pos_or_kw_1=1, pos_or_kw_2={"x": 1, "y": 1})
+
+
+def test_second_pos_or_kwarg_called_as_kwarg():
+    @takes(Point, name="pos_or_kw_2")
+    def test(pos_or_kw_1, pos_or_kw_2):
+        assert isinstance(pos_or_kw_2, Point)
+
+    test(1, pos_or_kw_2={"x": 1, "y": 1})
+
+
+def test_wrong_name_throws_at_decoration_time():
+    def test(args):
+        pass
+
+    with pytest.raises(ValueError) as exc:
+        takes(Point, name="missing")(test)
+        assert "not a valid argument" in str(exc.value)
+
+
+def test_decorated_function_called_with_incorrect_kwarg_throws():
+    @takes(Point, name="arg_1")
+    def test(arg_1):
+        pass
+
+    with pytest.raises(TypeError) as exc:
+        test(wrong=1)
+        assert "wrong" in str(exc.value)
+
+
+def test_decorated_function_with_positional_arg_called_with_incorrect_kwarg():
+    @takes(Point, name="arg_2")
+    def test(arg_1, arg_2):
+        pass
+
+    with pytest.raises(TypeError) as exc:
+        test(1, wrong=1)
+        assert "wrong" in str(exc.value)
